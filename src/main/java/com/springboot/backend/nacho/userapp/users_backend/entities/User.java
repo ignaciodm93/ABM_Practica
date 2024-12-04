8f613818292c8c4bed2fdf3d00fd191c5337a393
NachoPC <ignaciodm93@gmail.com>
2024-12-04 08:53:37 -0300

    clase rol, creacion de bd de (tabla roles y tabla intermedia) y atributo manytomany en user

--- src/main/java/com/springboot/backend/nacho/userapp/users_backend/entities/User.java
+++ src/main/java/com/springboot/backend/nacho/userapp/users_backend/entities/User.java
@@ -1,10 +1,20 @@
 package com.springboot.backend.nacho.userapp.users_backend.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -14,7 +24,7 @@ import jakarta.validation.constraints.Size;
 @Entity
 @Table(name  = "users")
 public class User {
-	//video 138
+	//video 150
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -40,7 +50,21 @@ public class User {
 	@NotBlank
 	@Column(name = "password")
 	private String password;
+	
+	@JsonIgnoreProperties({"handler", "hibernateLazyInitializer"})
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(
+	    name = "users_roles",
+	    joinColumns = {@JoinColumn(name = "user_id")},
+	    inverseJoinColumns = {@JoinColumn(name = "role_id")},
+	    uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "role_id"})}
+	)
+	private List<Role> roles;
 
+	public User() {
+		this.roles = new ArrayList<>();
+	}
+	
 	public Long getId() {
 		return id;
 	}
@@ -87,5 +111,13 @@ public class User {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
 	}
 }
